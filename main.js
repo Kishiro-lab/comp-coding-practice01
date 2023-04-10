ebef857a9790d510ab1fdd0e1eb030e9e004a5da
kishiro <the.yuhei@gmail.com>
2023-04-10 18:21:48 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+{
+    const open = document.getElementById('open');
+    const overlay = document.querySelector('.overlay');
+    const close = document.getElementById('close');
+    const ul = document.querySelector('.overlay-list');
+
+    open.addEventListener('click', () => {
+        overlay.classList.add('show');
+        open.classList.add('hide');
+    });
+
+    close.addEventListener('click', () => {
+        overlay.classList.remove('show');
+        open.classList.remove('hide');
+    });
+
+    ul.addEventListener('click', () => {
+      overlay.classList.remove('show');
+      open.classList.remove('hide');
+    });
+
+}
